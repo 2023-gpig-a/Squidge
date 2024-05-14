d358a1042d21e4ccf66984fd1b56dceb5017f951
Marks Polakovs <marks@markspolakovs.me>
2024-05-14 17:19:20 +0100

    Capture test results

--- e2e/test_frontend.py
+++ e2e/test_frontend.py
@@ -3,4 +3,4 @@ from playwright.sync_api import Page, expect
 
 def test_frontend_loads(page: Page):
     page.goto("http://localhost:8080/")
-    expect(page.get_by_role("heading", name="Drone statuses")).to_be_visible()
+    expect(page.get_by_role("heading", name="Drone Statuses")).to_be_visible()
